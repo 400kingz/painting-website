615e4c2390f97eed1bdda9ba659685382222afcc
Chris-Windows-Desktop <cmendez20@utexas.edu>
2020-09-20 19:03:30 -0500

    Refactored navbar responsiveness JS code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,35 +4,21 @@ const closeMenu = document.querySelector('#closeMenu');
 const navbarLinks = document.querySelectorAll('.navbar__link');
 
 burgerMenu.addEventListener('click', () => {
-  if (navbarRight.classList.contains('hidden')) {
-    navbarRight.classList.remove('hidden');
-    burgerMenu.classList.add('hidden');
-    closeMenu.classList.remove('hidden');
-    closeMenu.classList.add('reveal');
-    navbarRight.classList.add('reveal');
-  } else {
-    navbarRight.classList.add('hidden');
-  }
-});
+  navbarRight.classList.toggle('reveal');
+  burgerMenu.classList.toggle('hidden');
+  closeMenu.classList.toggle('reveal');
+})
 
 closeMenu.addEventListener('click', () => {
-  if (navbarRight.classList.contains('reveal')) {
-    navbarRight.classList.remove('reveal');
-    navbarRight.classList.add('hidden');
-    burgerMenu.classList.remove('hidden');
-    burgerMenu.classList.add('reveal');
-    closeMenu.classList.remove('reveal');
-    closeMenu.classList.add('hidden');
-  }
+  navbarRight.classList.toggle('reveal');
+  burgerMenu.classList.toggle('hidden');
+  closeMenu.classList.toggle('reveal');
 });
-
-console.log(navbarLinks);
 
 navbarLinks.forEach(element => {
   element.addEventListener('click', () => {
-    navbarRight.classList.add('hidden');
-    closeMenu.classList.add('hidden');
-    burgerMenu.classList.remove('hidden');
-    burgerMenu.classList.add('reveal');
+    navbarRight.classList.toggle('reveal');
+    closeMenu.classList.toggle('reveal');
+    burgerMenu.classList.toggle('hidden');
   });
 });
