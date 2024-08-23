60d6314991c68642dbf98bf2c22644a693307ef6
400Kingz <flurpro@gmail.com>
2024-08-23 18:22:07 -0500

    fixed

--- scripts/loader.js
+++ scripts/loader.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     loader.style.height = '100%';
 
-    loader.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+    loader.style.backgroundColor = '#565656';
 
     loader.style.display = 'flex';
 
@@ -27,7 +27,90 @@ document.addEventListener("DOMContentLoaded", function() {
     loader.style.transition = 'transform 1s ease-in-out';
 
     // Add a loading spinner or text if desired
-    loader.innerHTML = '<div>Loading...</div>';
+    loader.innerHTML = `<div class="easel">
+	<div class="canvas">
+		<div class="painting"></div>
+	</div>
+	<div class="shadow"></div>
+</div>
+<div class="bob">
+	<div class="head">
+		<div class="fro"></div>
+		<div class="face"></div>
+		<div class="eyes">
+			<div class="eye"></div>
+			<div class="eye"></div>
+		</div>
+		
+		<div class="beard"></div>
+		<div class="stash"></div>
+		<div class="mouth"></div>
+		<div class="nose"></div>
+	</div>
+	<div class="torso">
+		<div class="shirt"></div>
+		<div class="buttons"></div>
+		<div class="chest"></div>
+		<div class="collar">
+			<div class="left"></div>
+			<div class="right"></div>
+		</div>
+	</div>
+	<div class="legs">
+		<div class="leg left">
+			<div class="upper-leg"></div>
+			<div class="lower-leg">
+				<div class="shoe"></div>
+			</div>
+			
+		</div>
+		<div class="leg right">
+			<div class="upper-leg"></div>
+			<div class="lower-leg">
+				<div class="shoe"></div>
+			</div>
+		</div>
+		<div class="belt">
+			<div class="buckle"></div>
+		</div>
+	</div>
+	<div class="arm left">
+		<div class="upper-arm"></div>
+		<div class="lower-arm">
+			<div class="hand left-hand">
+				<div class="paint-brush"><div class="brush"></div></div>
+				<div class="thumb"></div>
+			</div>
+		</div>
+		
+	</div>
+	<div class="arm right">
+		<div class="upper-arm"></div>
+		<div class="lower-arm">
+			<div class="hand right-hand">
+				<div class="paints">
+					<div class="paint"></div>
+					<div class="paint"></div>
+					<div class="paint"></div>
+				</div>
+				<div class="thumb"></div>
+			</div>
+		</div>
+		
+	</div>
+	<div class="shadow"></div>
+</div>
+
+<div class="paint-splashes">
+	<div class="drip left"></div>
+	<div class="drip left"></div>
+	<div class="drip left"></div>
+</div>
+<div class="paint-splashes">
+	<div class="drip right"></div>
+	<div class="drip right"></div>
+	<div class="drip right"></div>
+</div>`;
 
     document.body.appendChild(loader);
 
